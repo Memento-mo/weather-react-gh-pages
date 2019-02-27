a6e5cdfdaac2cf330a8ef3d0d73b54f361e1d941
Memento-mo <gpologov@gmail.com>
2019-02-27 09:55:15 +0300

    Updates

---
+++ precache-manifest.c25533571a7ba54769d4ac633ccd9de3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0676c17d1714e2ff4656",
+    "url": "/weather-react-gh-pages/static/js/runtime~main.0676c17d.js"
+  },
+  {
+    "revision": "b94a109b1448aed66dc0",
+    "url": "/weather-react-gh-pages/static/js/main.b94a109b.chunk.js"
+  },
+  {
+    "revision": "bb473fd280a54f9a3adf",
+    "url": "/weather-react-gh-pages/static/js/2.bb473fd2.chunk.js"
+  },
+  {
+    "revision": "b94a109b1448aed66dc0",
+    "url": "/weather-react-gh-pages/static/css/main.bdfd6f5d.chunk.css"
+  },
+  {
+    "revision": "2b9d9ba1bc2ae9d15c904bebab5840f9",
+    "url": "/weather-react-gh-pages/index.html"
+  }
+];
